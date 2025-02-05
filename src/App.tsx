598d2d035acb0bd1d247311bef29c85db338a224
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-02-05 14:31:03 -0300

    SpeedInsights

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import FormularioFaltas from "./components/Formulario/formulario";
 import imagem from "./assets/imagem.jpg";
 import { Toaster } from "./components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       className="h-screen w-screen bg-cover flex justify-center items-center"
       style={{ backgroundImage: `url(${imagem})` }}
     >
+      <SpeedInsights />
       <Toaster position="top-right" duration={5000} />
       <FormularioFaltas />
     </div>
