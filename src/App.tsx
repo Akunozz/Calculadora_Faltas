7fb7b778b19203de3ac2ce538141fe863f9479f9
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-09-01 16:42:57 -0300

    remoção imagem de fundo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import FormularioFaltas from "./components/Formulario/formulario";
-import imagem from "./assets/imagem.jpg";
 import { Toaster } from "./components/ui/sonner"
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <div 
-      className="h-screen w-screen bg-cover flex justify-center items-center"
-      style={{ backgroundImage: `url(${imagem})` }}
+      className="h-screen w-screen bg-cover flex justify-center items-center bg-zinc-900"
     >
       <SpeedInsights />
       <Toaster position="top-right" duration={5000} />
